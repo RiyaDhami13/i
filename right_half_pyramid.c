e5f6591524d8e70e7a767953350832d9113be638
Riya Dhami <iamchoiceno.3@gmail.com>
2026-02-04 21:07:35 +0545

    Add right half pyramid solution

---
+++ right_half_pyramid.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  printf("Pattern program\n");
+  printf("Right Half pyramid\n");
+  printf("\nThis is using multiple printf statements");
+  printf("\n*");
+  printf("\n**");
+  printf("\n***");
+  printf("\n****");
+  printf("\n*****");
+  printf("\nThis is using single printf statement");
+  printf("\n*\n**\n***\n****\n*****");
+  return 0;
+}
